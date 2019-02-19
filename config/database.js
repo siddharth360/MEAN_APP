f451df5060046dc2b897b81687403512f6e0827f
siddharth chaudhary <siddharthchaudhary360@gmail.com>
2019-02-20 02:47:02 +0530

    mongodb

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   // configure the code below with your username, password and mlab database information 
-  //database: 'mongodb://siddharth:siddharth1@ds221155.mlab.com:21155/angularapp1',
-  database: 'mongodb://localhost:27017/meanProject',    //dev
+  database: 'mongodb://siddharth:siddharth1@ds221155.mlab.com:21155/angularapp1',
+  //database: 'mongodb://localhost:27017/meanProject'  
   secret: 'yoursecret'
 }
